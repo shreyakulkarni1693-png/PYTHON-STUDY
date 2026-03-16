3c4975533bf88e12e010f04093e8cb9a79dd8717
Shreya <shreyakulkarni1693@gmail.com>
2026-03-16 11:57:19 +0530

    added new file

--- 2-simpleExample.py
+++ 2-simpleExample.py
@@ -6,3 +6,7 @@ x=5 # This is also a comment
 y= "Hello, World!" # This is a comment too
 print(x)
 print(y)
+
+# use of end parameter in print function
+print("hello", end=" ")
+print("world")
---
+++ 3-variables.py
@@ -0,0 +1,40 @@
+# variables are used to store data in a program. 
+# They are like containers that hold values. 
+# In Python, you can create a variable by assigning a value to it.
+
+# Example of creating a variable
+x = 5
+y= "Hello, World!"
+
+print(type(x))   # integer
+print(type(y))   # string
+
+x= 4 
+x= "Python"
+print(x)   # takes last assigned value
+
+
+#type casting
+a=int(3)     #
+b=float(3)
+c=str(3)
+
+print(a)  
+print(b)   
+print(c)   
+
+
+
+# type() fuction is used to find the data type of a variable
+x= 5
+y="john"
+print(type(x))   # integer
+print(type(y))   # string
+
+
+#variables are case sensitive,
+
+A= 10 
+a= 20
+print(A)   # 10    
+print(a)   # 20
