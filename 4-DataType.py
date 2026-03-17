f4621a2403cf6dcce00e65583c72b52ec07e0192
Shreya <shreyakulkarni1693@gmail.com>
2026-03-17 10:22:30 +0530

    complete datatype study

--- 4-DataType.py
+++ 4-DataType.py
@@ -26,3 +26,102 @@ x=memoryview(bytes(5))   #memoryview
 x=None   #None type
 
 
+#python Numbers 
+
+x= 5      # int
+y= 5.2    # float
+z=1j      # complex number
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#integer is whole number, positive or negative,
+#  without decimals, of unlimited length.
+
+x=3
+y=145624789963221456622
+z=-4561236654456
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#float is a number, positive or negative, containing one or more decimals.
+
+x=3.42
+y=-3.32
+
+print(type(x))
+print(type(y))
+
+#float can also be scientific numbers with an "e" to indicate the power of 10.
+x=4E2
+y=3e3
+z=-34.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+print(x)
+print(y)    
+print(z)
+
+#complex numbers are written with a "j" as the imaginary part.
+
+x=4+6j
+y=8j
+z= -3j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+print(x)
+print(y)    
+print(z)
+
+#type conversion
+#you can convert from one type to another 
+
+x= 1      #int
+y= 2.3    #float
+z= 3j     #complex
+
+#convertfrom int to float 
+
+a=float(x)
+print(a)
+print(type(a))
+
+b=complex(x)
+print(b)    
+print(type(b))
+
+#convert from float to int
+
+c=int(y)
+print(c)
+print(type(c))
+
+d=complex(y)
+print(d)
+print(type(d))
+
+
+# can not convert complex to int and float
+
+
+# Random number
+#python has a built-in module called random that can be used to generate random numbers.
+
+#example
+#import the random module
+
+import random
+
+print(random.randrange(1,10))
+
+
+
